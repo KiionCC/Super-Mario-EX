3febb007ab1caaaf37ab17efa946b69673835e48
HumbabaZ <972230255@qq.com>
2017-06-20 23:28:27 +0800

    update
    6.20

--- skeleton.cpp
+++ skeleton.cpp
@@ -5,3 +5,9 @@ CCSize windowSize = CCDirector::sharedDirector()->getWinSize();
 skeletonNode->setPosition(ccp(windowSize.width / 2, 20));
 addChild(skeletonNode);
 skeletonNode->release();
+
+if (skeletonNode->states[0]->loop) {
+    if (skeletonNode->states[0]->time > 2) skeletonNode->setAnimation("jump", false);
+} else {
+    if (skeletonNode->states[0]->time > 1) skeletonNode->setAnimation("walk", true);
+}
