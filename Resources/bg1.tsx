1bedcdae12289f13296de41ce80331d5ae089bca
HumbabaZ <972230255@qq.com>
2017-06-21 01:26:30 +0800

    resources

---
+++ Resources/bg1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="background1" tilewidth="16" tileheight="16" tilecount="3240" columns="81">
+ <image source="bg1.png" width="1300" height="640"/>
+</tileset>
---
+++ Resources/bg2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="background2" tilewidth="16" tileheight="16" tilecount="3280" columns="82">
+ <image source="bg3.png" width="1312" height="640"/>
+</tileset>
